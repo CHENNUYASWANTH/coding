abd172320cc870f8af41a49d341fb6aadab17359
CHENNUYASWANTH <72649866+CHENNUYASWANTH@users.noreply.github.com>
2021-01-18 18:50:18 +0530

    Update floodfill.py

--- day1/floodfill.py
+++ day1/floodfill.py
@@ -1,12 +1,15 @@
-class Solution {
-   public  void visit(int[][] image,int or,int sr,int sc,int newcolor,int r,int c){
-       if(sr<0 || sr>=r || sc<0 || sc>=c || image[sr][sc]!=or)
+class Solution{
+   
+   public  void dfs(int[][] image,int or,int sr,int sc,int newcolor,int r,int c){
+       if(sr<0 || sr>=r || sc<0 || sc>=c || image[sr][sc]!=or){
            return;
+       }
        image[sr][sc]=newcolor;
-       visit(image,or,sr+1,sc,newcolor,r,c);     
-visit(image,or,sr,sc+1,newcolor,r,c); 
-visit(image,or,sr-1,sc,newcolor,r,c);
-visit(image,or,sr,sc-1,newcolor,r,c);
+      
+      dfs(image,or,sr+1,sc,newcolor,r,c);     
+      dfs(image,or,sr,sc+1,newcolor,r,c); 
+      dfs(image,or,sr-1,sc,newcolor,r,c);
+      dfs(image,or,sr,sc-1,newcolor,r,c);
    }
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
         if(newColor==image[sr][sc])
@@ -14,7 +17,7 @@ visit(image,or,sr,sc-1,newcolor,r,c);
         int r=image.length;
         int c=image[0].length;
         int or=image[sr][sc];
-        visit(image,or,sr,sc,newColor,r,c);
+        dfs(image,or,sr,sc,newColor,r,c);
 
         
       return image;  
